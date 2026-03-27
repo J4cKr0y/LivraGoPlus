1bfad11c288e0a0c8ccee51f9ffd4e855ae385de
J4cKr0y <kiky270281@gmail.com>
2026-03-27 22:45:37 +0100

    feat: install real ocr service - MlKit

--- src/core/di/AppDependencies.ts
+++ src/core/di/AppDependencies.ts
@@ -1,16 +1,20 @@
 import { DeliveryService } from '../services/DeliveryService';
 //import { InMemoryDeliveryRepository } from '../../infrastructure/storage/InMemoryDeliveryRepository';
 import { SQLiteDeliveryRepository } from '../../infrastructure/storage/SQLiteDeliveryRepository';
-import { FakeOcrService } from '../../infrastructure/ocr/FakeOcrService';
+//import { FakeOcrService } from '../../infrastructure/ocr/FakeOcrService';
 import { FakeMapService } from '../../infrastructure/map/FakeMapService';
 //import { FakeGeocodingService } from '../../infrastructure/geocoding/FakeGeocodingService';
 import { ExpoGeocodingService } from '../../infrastructure/geocoding/ExpoGeocodingService';
+import { MlKitOcrService } from '../../infrastructure/ocr/MlKitOcrService';
+
 
 export const initDependencies = () => {
  // const repository = new InMemoryDeliveryRepository();
 const repository = new SQLiteDeliveryRepository();
   
-  const ocrService = new FakeOcrService("123 Avenue de la République\n83000 Toulon");
+//  const ocrService = new FakeOcrService("123 Avenue de la République\n83000 Toulon");
+ 
+const ocrService = new MlKitOcrService();
   
   const mapService = new FakeMapService();
 const geocodingService = new ExpoGeocodingService();
